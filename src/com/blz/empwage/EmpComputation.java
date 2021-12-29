67e69d274a5af186e9f1ed948d8b539b04f3c2ec
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-29 14:59:20 +0530

    Added : Program for Emp Wage Using Switch

--- src/com/blz/empwage/EmpComputation.java
+++ src/com/blz/empwage/EmpComputation.java
@@ -2,28 +2,28 @@ package com.blz.empwage;
 
 public class EmpComputation {
 
-	public static final int IS_PRESENT = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int SALARY_PER_HR = 20;
+	public static final int IS_FULL_TIME = 1;
+    public static final int SALARY_PER_HR = 20;
+    public static final int IS_PART_TIME = 2;
 
 	public static void employeeAttendace() {
 
-		double empCheck = Math.floor((Math.random() * 10) % 2);
-		int Working_Hr = 0;
+		int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+        int Working_Hr = 0;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                Working_Hr = 8;
+                break;
+            case IS_PART_TIME:
+                Working_Hr = 4;
+                break;
+            default:
+                Working_Hr = 0;
+        }
+        double Salary = Working_Hr * SALARY_PER_HR;
+        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
+    }
 
-		if (empCheck == IS_PRESENT) {
-			Working_Hr = 8;
-		}
-		 else if (empCheck == IS_PART_TIME) {
-	            Working_Hr = 4;
-	        }
-	        else
-	        {
-	            Working_Hr = 0;
-	        }
-	        double Salary = Working_Hr * SALARY_PER_HR;
-	        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
-		}
 
 		public static void main(String[] args) {
 		EmpComputation.employeeAttendace();
