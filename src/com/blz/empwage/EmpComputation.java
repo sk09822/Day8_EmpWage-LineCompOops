046498e469224776ed9f3a65f798b619c41d004e
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-29 14:00:04 +0530

    Added : Program for calcualte Daily Employee Wage

--- src/com/blz/empwage/EmpComputation.java
+++ src/com/blz/empwage/EmpComputation.java
@@ -1,23 +1,23 @@
 package com.blz.empwage;
 
 public class EmpComputation {
-
+	
+	public static final int IS_PRESENT = 1;
+    public static final int SALARY_PER_HR = 20;
 	
 		public static void employeeAttendace() 
 		{
-		        int IS_PRESENT = 1;
-		        double empCheck = Math.floor((Math.random() * 10) % 2);
+			
 
-		        if (empCheck == IS_PRESENT)
+		    double empCheck = Math.floor((Math.random() * 10) % 2);
+		    int Working_Hr = 0;
+				if (empCheck == IS_PRESENT)
 		        {
-
-		            System.out.println("Employee is Present");
-		        }
-		        else
-		        {
-		            System.out.println("Employee is Absent");
-		        }
+		        Working_Hr = 8;
 		    }
+		    double Salary = Working_Hr * SALARY_PER_HR;
+				System.out.println("Employee Salary is = " + Salary + "  Doller");
+		}
 		
 
     public static void main(String[] args)
