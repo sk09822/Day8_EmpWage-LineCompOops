13ec7d4fe433d7a348597efe37aff8202baaff3d
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-29 14:50:25 +0530

    Added : Program for calcualte Part Time Employee Wage

--- src/com/blz/empwage/EmpComputation.java
+++ src/com/blz/empwage/EmpComputation.java
@@ -1,30 +1,32 @@
 package com.blz.empwage;
 
 public class EmpComputation {
-	
+
 	public static final int IS_PRESENT = 1;
-    public static final int SALARY_PER_HR = 20;
-	
-		public static void employeeAttendace() 
-		{
-			
+	public static final int IS_PART_TIME = 2;
+	public static final int SALARY_PER_HR = 20;
 
-		    double empCheck = Math.floor((Math.random() * 10) % 2);
-		    int Working_Hr = 0;
-				if (empCheck == IS_PRESENT)
-		        {
-		        Working_Hr = 8;
-		    }
-		    double Salary = Working_Hr * SALARY_PER_HR;
-				System.out.println("Employee Salary is = " + Salary + "  Doller");
+	public static void employeeAttendace() {
+
+		double empCheck = Math.floor((Math.random() * 10) % 2);
+		int Working_Hr = 0;
+
+		if (empCheck == IS_PRESENT) {
+			Working_Hr = 8;
 		}
-		
+		 else if (empCheck == IS_PART_TIME) {
+	            Working_Hr = 4;
+	        }
+	        else
+	        {
+	            Working_Hr = 0;
+	        }
+	        double Salary = Working_Hr * SALARY_PER_HR;
+	        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
+		}
 
-    public static void main(String[] args)
-    {
-    	EmpComputation.employeeAttendace();
-    }
-
+		public static void main(String[] args) {
+		EmpComputation.employeeAttendace();
 	}
 
-
+}
