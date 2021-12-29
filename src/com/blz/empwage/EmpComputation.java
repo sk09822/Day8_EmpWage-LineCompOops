9e5c28eb2de29c869e4bc4dfec1f40d50a284a88
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-29 15:19:50 +0530

    Added : Program for Refractor the Program add the Method

--- src/com/blz/empwage/EmpComputation.java
+++ src/com/blz/empwage/EmpComputation.java
@@ -2,41 +2,46 @@ package com.blz.empwage;
 
 public class EmpComputation {
 
-	public static final int IS_FULL_TIME = 1;
-	public static final int SALARY_PER_HR = 20;
-	public static final int IS_PART_TIME = 2;
-	public static final int NO_OF_DAYS = 20;
-	public static final int MAX_HR_IN_MONTH = 100;
+	static final int IS_PRESENT = 1;
+	static final int IS_PART_TIME = 2;
 
-	public static void employeeAttendace() {
-		int Working_Hr = 0;
-		int TotalEmpHrs = 0;
-		int TotalEmpWorkingDay = 0;
-		while (TotalEmpHrs <= MAX_HR_IN_MONTH && TotalEmpWorkingDay < NO_OF_DAYS) {
-			TotalEmpWorkingDay++;
-			int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+	public static void employeeAttendace(int SALARY_PER_HOUR, int TOTAL_WORK_DAY, int MAX_WORKING_HOUR,
+			int FULL_DAY_WORKING_HOUR) {
+		int empCheck;
+		int empHr = 0;
+		int totalSalary = 0;
+		int day = 0;
+
+		while (day < TOTAL_WORK_DAY && (empHr + FULL_DAY_WORKING_HOUR) <= MAX_WORKING_HOUR) {
+			day++;
+			empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 
 			switch (empCheck) {
-			case IS_FULL_TIME:
-				Working_Hr = 8;
+			case IS_PRESENT:
+				empHr += FULL_DAY_WORKING_HOUR;
 				break;
-			case IS_PART_TIME:
-				Working_Hr = 4;
-				break;
-			default:
-				Working_Hr = 0;
-			}
-			TotalEmpHrs += Working_Hr;
-			System.out.println("Day :  " + TotalEmpWorkingDay + "   Emp Hr : " + Working_Hr);
-		}
-		int totalEmpWage = TotalEmpHrs * SALARY_PER_HR;
 
-		System.out.println("Total Emp Wage : " + totalEmpWage);
+			case IS_PART_TIME:
+				empHr += (FULL_DAY_WORKING_HOUR / 2);
+				break;
+
+			default:
+				empHr += 0;
+				break;
+			}
+
+			System.out.println("Working day : " + day + " (Present : " + empCheck + ")");
+
+		}
+
+		totalSalary = empHr * SALARY_PER_HOUR;
+		System.out.println("Monthly Emp Wage : " + totalSalary + "\nTotal working hour : " + empHr);
+
 	}
 
 	public static void main(String[] args) {
 
-		EmpComputation.employeeAttendace();
+		EmpComputation.employeeAttendace(20, 20, 100, 8);
 	}
 
 }
