95af30a00cf3c15f97182626828568910f85cd78
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-29 15:06:49 +0530

    Added : Program for Calculate Wage For Month

--- src/com/blz/empwage/EmpComputation.java
+++ src/com/blz/empwage/EmpComputation.java
@@ -3,29 +3,39 @@ package com.blz.empwage;
 public class EmpComputation {
 
 	public static final int IS_FULL_TIME = 1;
-    public static final int SALARY_PER_HR = 20;
-    public static final int IS_PART_TIME = 2;
+	public static final int SALARY_PER_HR = 20;
+	public static final int IS_PART_TIME = 2;
+	public static final int NO_OF_DAYS = 20;
 
 	public static void employeeAttendace() {
 
-		int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-        int Working_Hr = 0;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                Working_Hr = 8;
-                break;
-            case IS_PART_TIME:
-                Working_Hr = 4;
-                break;
-            default:
-                Working_Hr = 0;
-        }
-        double Salary = Working_Hr * SALARY_PER_HR;
-        System.out.println("Emplyee Salary is = " + Salary + "  Doller");
-    }
+		int Working_Hr = 0;
+		int empwage = 0;
+		int Totalempwage = 0;
 
+		for (int day = 0; day < NO_OF_DAYS; day++) {
+			int empCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-		public static void main(String[] args) {
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				Working_Hr = 8;
+				break;
+			case IS_PART_TIME:
+				Working_Hr = 4;
+				break;
+			default:
+				Working_Hr = 0;
+			}
+			empwage = Working_Hr * SALARY_PER_HR;
+			Totalempwage += empwage;
+			System.out.println("Emp Wage :  " + empwage);
+		}
+
+		System.out.println("Total Emp Wage : " + Totalempwage);
+	}
+
+	public static void main(String[] args) {
+
 		EmpComputation.employeeAttendace();
 	}
 
